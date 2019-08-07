ea29c56324b8ff941042fbc91ec621a43a67995c
cramallo <cramallo65@gmail.com>
2019-08-07 15:43:07 -0300

    findOneByUsername in userRepository

--- src/main/java/com/wolox/training/repositories/UserRepository.java
+++ src/main/java/com/wolox/training/repositories/UserRepository.java
@@ -3,5 +3,5 @@ import com.wolox.training.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
-    User findByUsername(String username);
+    User findOneByUsername(String username);
 }
