2b4f847aa6e226b18231bb83c2a6fe8b010cbd55
cramallo <cramallo65@gmail.com>
2019-08-15 17:43:21 -0300

    test conext error solved

--- src/main/java/com/wolox/training/TrainingApplication.java
+++ src/main/java/com/wolox/training/TrainingApplication.java
@@ -3,7 +3,7 @@ package com.wolox.training;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.wolox.training.services"})
 public class TrainingApplication {
 
 	public static void main(String[] args) {
