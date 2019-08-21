1766a1ba21226b2b14a0ef783416fc6247d9627e
cramallo <cramallo65@gmail.com>
2019-08-21 12:44:54 -0300

    adding authentication facade interface

---
+++ src/main/java/com/wolox/training/security/IAuthenticationFacade.java
@@ -0,0 +1,6 @@
+package com.wolox.training.security;
+import org.springframework.security.core.Authentication;
+
+public interface IAuthenticationFacade {
+    Authentication getAuthentication();
+}
